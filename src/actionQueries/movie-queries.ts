c67765acdb88e0236b35362f236a02920c14bcdf
Steven Wickers <StevenWickers@gmail.com>
2025-01-21 07:12:26 -0500

    added gitignore file

--- src/actionQueries/movie-queries.ts
+++ src/actionQueries/movie-queries.ts
@@ -3,6 +3,7 @@ import * as MovieActions from '../actions/movie-action'
 
 export const MOVIE_ENDPOINT = 'movies'
 
+
 export const fetchMovies = async (dispatch: any, token:string) => {
   await getData(MOVIE_ENDPOINT, MovieActions.loadMovies, dispatch, token)
 }
